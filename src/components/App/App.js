8fd3ebe2e744c8afc93fc18d0ca140ee67d37108
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-12 20:34:39 -0300

    agrega ws

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import { w3cwebsocket } from 'websocket'
 
-function App() {
+const client = new w3cwebsocket('ws://192.168.0.17/echo', 'message')
+
+const App = () => {
+
+  const [mensaje, setMensaje] = useState('Nada')
+  const [datosWiiBalance, setDatosWiiBalance] = useState({})
+
+  const conectarConRaspberryPi = () => {
+    setMensaje('Conectando...')
+    client.onerror = () => {
+      setMensaje('Error: No se encontró una Raspberry Pi en la URL ingresada.')
+    }
+    client.onopen = () => {
+      setMensaje('Conexión exitosa con Rasberry Pi')
+      client.send('iniciar')
+    }
+    client.onclose = () => {
+      setMensaje('Conexión cerrada')
+      setDatosWiiBalance({})
+    }
+    client.onmessage = e => {
+      if (typeof e.data === 'string') {
+        setDatosWiiBalance(JSON.parse(e.data))
+      }
+    }
+  }
+
   return (
     <div className="App">
-      x
+      <p>{mensaje}</p>
+      {JSON.stringify(datosWiiBalance)}
+      <button onClick={conectarConRaspberryPi}>Conectar</button>
     </div>
   )
 }
