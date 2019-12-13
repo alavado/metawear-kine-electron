a3b2341c74b415a4ee505764efac03aa1fcbd622
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-12 22:41:39 -0300

    mejora interfaz para cuaterniones

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ const client = new w3cwebsocket('ws://192.168.0.17/echo', 'message')
 const App = () => {
 
   const [mensaje, setMensaje] = useState('Nada')
-  const [datosWiiBalance, setDatosWiiBalance] = useState({})
+  const [dispositivos, setDispositivos] = useState({})
 
   const conectarConRaspberryPi = () => {
     setMensaje('Conectando...')
@@ -16,23 +16,45 @@ const App = () => {
     }
     client.onopen = () => {
       setMensaje('Conexión exitosa con Rasberry Pi')
-      client.send('iniciar')
     }
     client.onclose = () => {
       setMensaje('Conexión cerrada')
-      setDatosWiiBalance({})
+      setDispositivos({})
     }
     client.onmessage = e => {
       if (typeof e.data === 'string') {
-        setDatosWiiBalance(JSON.parse(e.data))
+        setDispositivos(JSON.parse(e.data))
       }
     }
   }
 
+  const macs = Object.keys(dispositivos)
+
   return (
     <div className="App">
       <p>{mensaje}</p>
-      {JSON.stringify(datosWiiBalance)}
+      <table>
+        <thead>
+          <th>MAC</th>
+          <th>w</th>
+          <th>x</th>
+          <th>y</th>
+          <th>z</th>
+        </thead>
+        <tbody>
+          {macs.length > 0 &&
+            macs.map(mac => {
+              return <tr>
+                <td>{mac}</td>
+                <td>{dispositivos[mac][0].toLocaleString('de-DE')}</td>
+                <td>{dispositivos[mac][1].toLocaleString('de-DE')}</td>
+                <td>{dispositivos[mac][2].toLocaleString('de-DE')}</td>
+                <td>{dispositivos[mac][3].toLocaleString('de-DE')}</td>
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
       <button onClick={conectarConRaspberryPi}>Conectar</button>
     </div>
   )
